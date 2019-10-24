02c7c27c85991fdd7e102c1d1578422ed4b25323
gabe flavin <gabeflavin@gmail.com>
2019-10-24 12:49:22 -0400

    new updates - animation and directors page, some formatting

--- app/js/main.js
+++ app/js/main.js
@@ -15,17 +15,51 @@ $(document).ready(function(){
         }
     };
     
+    
+    
+    // LIGHTBOX OPEN 
+    var smileAnimation = new TimelineMax({paused: true, onComplete:smileAnimationDone, repeat:0, repeatDelay:0, yoyo:false});
+    
+    smileAnimation.to('.noteeth', 1, {opacity: 1, ease:Power3.easeOut}, 1)   
+     
+    .to('.smile_teeth', .5, {opacity: 1, ease:Power3.easeOut}, "-=.1")
+    
+    .to('.green', .2, {opacity: 1, ease:Power3.easeOut}, "+=.4")
+    
+    .to('.logo', .6, {opacity: 1, ease:Power3.easeInOut}, "+=.5")
+    
+    .to('.home_nav', 1.2, {y: -25, opacity: 1, ease:Power1.easeInOut}, "+=.4");
+    
+    
+    function smileAnimationDone(){}
 
+    
+    var url1 = 'https://player.vimeo.com/video/211748556?app_id=122963';
+    var url2 = 'https://player.vimeo.com/video/264459449?app_id=122963';
+    
 
-    $('a.backtotop').bind('click',function(event){
+    $('a.left').bind('click',function(event){
         event.preventDefault();
-        TweenMax.to(window, 1.1, {scrollTo:0, ease:Cubic.easeInOut});
+        nextVid(url1);
     });
     
+    $('a.right').bind('click',function(event){
+        event.preventDefault();
+        nextVid(url2);
+    });
+    
+    function nextVid(n){
+        TweenMax.to('.video1', .5, {opacity:0, ease:Power4.easeOut, onComplete:switchVid}, 0);
+        function switchVid(){
+            $('.video1').prop('src', n);
+            TweenMax.to('.video1', .5, {opacity:1, ease:Power4.easeInOut, delay: .5});
+        }
+    }
       
     
     window.onload = function() {
         TweenMax.to('body', .5, {opacity:1, ease:Power4.easeOut});
+        smileAnimation.restart();
     };
     
     
